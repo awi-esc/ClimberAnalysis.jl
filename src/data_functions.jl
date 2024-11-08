34b5993bc52b6fb2733d6b812e13a72edfa5570b
Anna Höse <anna.hoese@awi.de>
2024-11-08 11:03:22 +0100

    New funtcions to get variables from data in DimArray format

--- src/data_functions.jl
+++ src/data_functions.jl
@@ -4,6 +4,31 @@ using NetCDF
 using DimensionalData
 using CommonDataModel
 
+
+function getAveragedVar(data, varname::String, mean::Bool=true)
+    if mean
+        var= Array(data[varname][:,:,13,end])
+    else
+        var=nothing
+    end
+
+    lon = data["lon"];
+    lat = data["lat"];
+    outdata = DimArray(Array(var), (Dim{:lon}(lon), Dim{:lat}(lat)))
+
+    return outdata
+end
+
+function getAnomVar(data, varname::String, ind_year::Number; ref_year::Number=1)
+    val1= Array(data[varname][:,:,13,ind_year])
+    val2= Array(data[varname][:,:,13,ref_year])
+    lon = data["lon"];
+    lat = data["lat"];
+    data1 = DimArray(Array(val1), (Dim{:lon}(lon), Dim{:lat}(lat)))
+    data2 = DimArray(Array(val2), (Dim{:lon}(lon), Dim{:lat}(lat)))
+    return data1-data2
+end
+
 function getYearlyMean(data::CommonDataModel.CFVariable)
     data_yearly=data[:,:,13,:]
     return data_yearly
@@ -38,5 +63,3 @@ function getSeasonalMean(data::CommonDataModel.CFVariable, season::String, yeari
     end    
     return data_out
 end
-
-
